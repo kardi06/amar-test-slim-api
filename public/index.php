b117d109fced7b1a70c2d21fb8e66e62c3a518d0
Kardi <kgland06@gmail.com>
2024-09-06 21:18:06 +0700

    basic

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<?php
+
+use Slim\Factory\AppFactory;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->get('/', function (Request $request, Response $response){
+    $response->getBody()->write('Hello World');
+
+    return $response;   
+});
+
+$app->run();
